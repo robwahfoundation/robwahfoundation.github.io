b619dc5b83cc1d3cc515a7c40538c9ca5795e2fe
pratomoadi <tommyatmaji@gmail.com>
2021-12-19 02:26:07 +0700

    adding content and validation when video ended

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,4 +1,17 @@
-$(document).on("click", "#video_play_button, #video_pause_button", function () {
+$(document).on(
+  "click",
+  "#video_play_button, #video_pause_button",
+  setPlayOrPauseBtn
+);
+
+var aud = document.getElementById("videoPlayer");
+aud.onended = function () {
+  $("#video_play_button").removeAttr("hidden");
+  $("#video_pause_button").attr("hidden", true);
+  $("#videoPlayer").trigger("pause");
+};
+
+function setPlayOrPauseBtn() {
   if ($("#video_pause_button").attr("hidden")) {
     $("#video_pause_button").removeAttr("hidden");
     $("#video_play_button").attr("hidden", true);
@@ -8,4 +21,4 @@ $(document).on("click", "#video_play_button, #video_pause_button", function () {
     $("#video_pause_button").attr("hidden", true);
     $("#videoPlayer").trigger("pause");
   }
-});
+}
