55c29903c50f79b342eb5d05761ec4c8efb71e21
pratomoadi <tommyatmaji@gmail.com>
2022-02-25 07:12:08 +0700

    Menambahkan form kontak kami

--- assets/js/contact.js
+++ assets/js/contact.js
@@ -1,6 +1,36 @@
 $(document).ready(function () {
   $("#submitBtn").on("click", function (e) {
-    alert("test");
+    let gender = $("#gender").val();
+    let phone = "6285649751560";
+    let success = true;
+    if (gender == "female") {
+      phone = "6285735012063";
+    }
+    let formSubmit = $(".php-email-form .form-control");
+    $.each(formSubmit, function (item, index) {
+      if (index.value == "") {
+        alert(index.name + " Anda Tidak Boleh Kosong!");
+        success = false;
+      }
+    });
+    if (success) {
+      let url =
+        "https://api.whatsapp.com/send?phone=" +
+        phone +
+        "&text=Nama Pengirim Pesan: " +
+        $("#name").val() +
+        "%0a" +
+        "Email / Kontak WA Pengirim Pesan: " +
+        $("#email").val() +
+        "%0a" +
+        "Topik Perihal: " +
+        $("#subject").val() +
+        "%0a" +
+        "Pesan yang Disampaikan: " +
+        $("#message").val();
+      location.href = url;
+    }
+
     e.preventDefault();
   });
 });
